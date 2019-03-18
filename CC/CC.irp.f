ebe478a8bc15f3519fb67f28cf694d463696d8c9
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2019-03-18 15:21:35 +0100

    build core structure of the plugin

--- CC/CC.irp.f
+++ CC/CC.irp.f
@@ -1,7 +1,20 @@
 program CC
   implicit none
   BEGIN_DOC
-! TODO : Put the documentation of the program here
+  ! CC
+  ! Coupled cluster
+  ! Coupled cluster
+  ! This program perform
   END_DOC
-  print *, 'Hello world'
+  PROVIDE cc_mode
+  select case ( trim(cc_mode) )
+    case ( 'CCD' )
+    call CCD
+    case ( 'CCSD' )
+    call CCSD
+    case ( 'CCSDT' )
+    call CCSDT
+    case default
+    print *, "Error noa", trim(cc_mode),"available"
+  end select
 end
